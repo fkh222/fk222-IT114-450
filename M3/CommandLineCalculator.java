b40ab6c23753c1d4e49083fd10852e97b01766fe
Fariha K <fk222@njit.edu>
2025-06-18 23:16:17 -0400

    solved command line calculator problem

--- M3/CommandLineCalculator.java
+++ M3/CommandLineCalculator.java
@@ -12,7 +12,7 @@ Challenge 1: Command-Line Calculator
 */
 
 public class CommandLineCalculator extends BaseClass {
-    private static String ucid = "mt85"; // <-- change to your ucid
+    private static String ucid = "fk222"; // <-- change to your ucid
 
     public static void main(String[] args) {
         printHeader(ucid, 1, "Objective: Implement a calculator using command-line arguments.");
@@ -25,16 +25,40 @@ public class CommandLineCalculator extends BaseClass {
 
         try {
             System.out.println("Calculating result...");
+            // fk222 6/18/25
+
             // extract the equation (format is <num1> <operator> <num2>)
+            String first = args[0];
+            String operator = args[1];
+            String second = args[2];
+
+            // count amount of decimal places in arguments
+
+            int decimalCount1 = first.contains(".") ? first.length() - first.indexOf('.') - 1 : 0;    
+            int decimalCount2 = second.contains(".") ? second.length() - second.indexOf('.') - 1 : 0;    
+            int resultDecimalCount = Math.max(decimalCount1, decimalCount2);
 
             // check if operator is addition or subtraction
-
+            if (!operator.equals("+") && !operator.equals("-")){
+                System.out.println("Invalid operator. Program only supports addition or subtraction for now.");
+                printFooter(ucid, 1);
+                return;
+            }
             // check the type of each number and choose appropriate parsing
-
+            Number num1 = first.contains(".") ? Double.parseDouble(first) : Integer.parseInt(first);
+            Number num2 = second.contains(".") ? Double.parseDouble(second) : Integer.parseInt(second);
+            
             // generate the equation result (Important: ensure decimals display as the
             // longest decimal passed)
             // i.e., 0.1 + 0.2 would show as one decimal place (0.3), 0.11 + 0.2 would shows
-            // as two (0.31), etc
+            // as two (0.31), etc      
+            double n1 = num1.doubleValue();
+            double n2 = num2.doubleValue();
+            double result = operator.equals("+") ? n1 + n2 : n1 - n2;
+
+            String format = "%." + resultDecimalCount + "f";
+            System.out.printf("= " + format, result);
+            System.out.println();
 
         } catch (Exception e) {
             System.out.println("Invalid input. Please ensure correct format and valid numbers.");
