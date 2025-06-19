15fd061c7854000d5d89e2dcb4414dac0f0ec2ab
Fariha K <fk222@njit.edu>
2025-06-19 03:48:52 -0400

    solved mad libs generator problem

--- M3/MadLibsGenerator.java
+++ M3/MadLibsGenerator.java
@@ -18,7 +18,7 @@ Challenge 3: Mad Libs Generator (Randomized Stories)
 
 public class MadLibsGenerator extends BaseClass {
     private static final String STORIES_FOLDER = "M3/stories";
-    private static String ucid = "mt85"; // <-- change to your ucid
+    private static String ucid = "fk222"; // <-- change to your ucid
 
     public static void main(String[] args) {
         printHeader(ucid, 3,
@@ -37,11 +37,41 @@ public class MadLibsGenerator extends BaseClass {
         // Start edits
 
         // load a random story file
-
+        File[] stories = folder.listFiles();
+        int randomNum = (int)(Math.random()*stories.length);
+        File story = stories[randomNum];
         // parse the story lines
-
+        try (Scanner fileScanner = new Scanner(story)){
+            while (fileScanner.hasNextLine()){
+                lines.add(fileScanner.nextLine());
+            }
+        } catch (Exception e){
+            System.out.println("File could not be read");
+            return;
+        }
         // iterate through the lines
+        for (int i=0; i<lines.size(); i++){
+            String line = lines.get(i);
+            String newLine = "";
+            int start = 0;
+            while(line.indexOf("<", start) != -1 && line.indexOf(">", start) != -1){ //checks beginning and end of placeholder
+                int placeholderStart = line.indexOf("<", start);
+                int placeholderEnd = line.indexOf(">", placeholderStart);
 
+                newLine += line.substring(start, placeholderStart); //this adds the previous text
+
+                String placeholder = line.substring(placeholderStart + 1, placeholderEnd);
+                placeholder = placeholder.replace("_", " ");
+                System.out.println("Enter -" + placeholder + ": ");
+                String userInput = scanner.nextLine();
+
+                newLine += userInput; //update line with user's input
+
+                start = placeholderEnd + 1; // update start index to after current word
+            }
+            newLine += line.substring(start); //add rest of line back in
+            lines.set(i, newLine); // finally update line with new version.
+        }
         // prompt the user for each placeholder (note: there may be more than one
         // placeholder in a line)
 
